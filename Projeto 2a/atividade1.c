412ca8a26c430b83d85c6f52d237d9a320543e8b
Duda <106630667+mdudag@users.noreply.github.com>
2024-04-06 21:56:05 -0300

    Update atividade1.c

--- Projeto 2a/atividade1.c
+++ Projeto 2a/atividade1.c
@@ -1,40 +1,40 @@
-// Discente: Maria Eduarda Guedes Alves    
+// Atividade 1: Analisador Lexico de Binarios
+//    Discente: Maria Eduarda Guedes Alves 
     
 #include <stdio.h>
-#define N 4
 
 int main(void) {
-    FILE *f; 
-    char token[N], c;
+    FILE *f; char c;
 
-    f = fopen("C:/Users/maria/OneDrive/Documentos/_UESC/Aulas/Conceitos de LP/Projeto 2a/arquivo.txt", "r");    
-                                    // Abre arquivo
-    if (!f) {                       // Verificando se o arquivo foi aberto corretamente
+    f = fopen("C:/Users/maria/OneDrive/Documentos/_UESC/Aulas/Conceitos de LP/Projeto 2a/file1.txt", "r");    
+    
+    // Verificando se o arquivo foi aberto corretamente
+    if (!f) {                       
         printf("Erro ao abrir arquivo!");
         return 1;
     }
 
-    int i;
+    printf("\n========== Analisador Lexico de Binarios ==========\n");
+
     Q0: 
-        // Verificando se a linha nao e NULL
-        if (!fgets(token, sizeof(token), f)) {
+        // Verificando se nao chegou em fim de arquivo
+        if ((c = fgetc(f)) == EOF) {   
+            printf("\n\n");
             fclose(f); return 0;
         }
 
-        token[N-1] = '\0';  // Fim de string
-        i=0;                // Contador para as posicoes da string
-    Q1:
-        c = token[i];
-        ++i;
-
-        if (c=='\0') goto Q0;   // Pula para Q0 se chegou no final da string
-        else if (c=='0' || c=='1') {
+        if (c=='0' || c=='1') {
             printf("\n%c - Reconhecida!", c); 
-            goto Q1;
+            goto Q0;
         } 
+        // Se chegou no fim da linha, ler proxima
+        else if (c=='\n') {
+            printf("\n");
+            goto Q0;
+        }      
         else goto ERRO;
 
     ERRO: 
-        printf("\n%c - Nao reconhecida.", c);
+        printf("\n%c - Nao reconhecida.\n\n", c);
         fclose(f); return 1; 
 }
