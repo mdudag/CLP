4b929613b6ba2c2840a7e3128f8bdaea7a5d9ac1
Duda <106630667+mdudag@users.noreply.github.com>
2024-06-18 15:46:58 -0300

    Update ackermann.pl

--- Projeto 4a/atividade1/ackermann.pl
+++ Projeto 4a/atividade1/ackermann.pl
@@ -1,13 +1,29 @@
 % Discente Maria Eduarda Guedes Alves
 
-ack(M, N, 0, R) :- R is M+N.
-ack(M, 0, 1, 0) :- M>0.
-ack(M, 0, 2, M) :- M>0.
-ack(M, N, 2, R) :- M>0, N>0, R is M^N.
-ack(M, N, P, R) :- P>2, R is P.
-ack(M, N, P, R) :- M>0, N>0, P>0, 
+ack(M, N, 0, R) :- M>=0, N>=0, R is M+N.
+ack(M, 0, 1, R) :- M>=0, R is 0.
+ack(M, 0, 2, R) :- M>=0, R is 1.
+ack(M, N, 2, R) :- M>=0, N>=0, R is M^N.
+ack(M, N, P, R) :- M>=0, N>=0, P>2, R is P.
+ack(M, N, P, R) :- M>=0, N>0, P>0, 
     			   ack(M, N-1, P, Temp), 
                    ack(M, Temp, P-1, R).
 
-% Exemplo de consulta
-% ack(2, 3, 2, R), write('Ack(2, 3, 2) = '), write(R), nl.
+
+
+% Utilizei essa função que encontrei para calcular as possibilidades
+main :-
+    I = 9,
+    findall([M, N, P], (between(0, I, M), between(0, I, N), between(0, I, P)), Combinations),
+    process_combinations(Combinations).
+
+% Processar todas as combinações de valores
+process_combinations([]).
+process_combinations([[M, N, P] | Rest]) :-
+    (ack(M, N, P, R) -> 
+        format('ack(~d, ~d, ~d): ~d\n', [M, N, P, R]);
+        true),
+    process_combinations(Rest).
+:- main.
+
+% Consulta: main.
